6bf8f3e2f51794eaf9b792eecb3ca64e684bf558
Allen Lee <allen870619@gmail.com>
2024-04-08 19:55:54 +0800

    Introduce WeSplit app

---
+++ WeSplit/WeSplit/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Allen Lee on 2024/4/8.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Allen Lee on 2024/4/8.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
