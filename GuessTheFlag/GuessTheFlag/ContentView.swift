fe9a5808f3ef03e0333bd3f2c909b74e78ee079b
Allen Lee <allen870619@gmail.com>
2024-04-14 20:21:00 +0800

    Introduce guess the flag

---
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  GuessTheFlag
+//
+//  Created by Allen Lee on 2024/4/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack(alignment: .leading, spacing: 20) {
+            Spacer()
+            Text("Hello, world!")
+            Text("This is another text view")
+            Spacer()
+            Spacer()
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GuessTheFlag/GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Allen Lee on 2024/4/14.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
