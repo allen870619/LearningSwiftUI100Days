f9bb95c4732fb0a4959d5b192ee19c8b7fdd4b3e
Allen Lee <allen870619@gmail.com>
2024-04-16 23:51:29 +0800

    Add projects2's image assets and buttons

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -8,28 +8,52 @@
 import SwiftUI
 
 struct ContentView: View {
+    var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"]
+    var correctAnswer = Int.random(in: 0...2)
+    
     var body: some View {
-        VStack(alignment: .leading, spacing: 20) {
-            Text("Hello, world!")
-            Color.green
-                .frame(width: 200, height: 200)
-            Text("This is another text view")
-                .background(.blue)
-            ZStack {
-                VStack(spacing: 0) {
-                    Color.red
-                    Color.blue
+        ZStack {
+            Color.blue
+                .ignoresSafeArea()
+            
+            VStack(spacing: 30) {
+                Text("Tap the flag of")
+                    .foregroundStyle(.white)
+                Text(countries[correctAnswer])
+                    .foregroundStyle(.red)
+                
+                ForEach(0..<3) { num in
+                    Button {
+                        // action
+                    } label: {
+                        Image(countries[num])
+                    }
                 }
-
-                Text("Your content")
-                    .foregroundStyle(.secondary)
-                    .padding(50)
-                    .background(.ultraThinMaterial)
             }
         }
-        .background(.red)
-        .padding()
-        .ignoresSafeArea()
+        
+        
+//        VStack(alignment: .leading, spacing: 20) {
+//            Text("Hello, world!")
+//            Color.green
+//                .frame(width: 200, height: 200)
+//            Text("This is another text view")
+//                .background(.blue)
+//            ZStack {
+//                VStack(spacing: 0) {
+//                    Color.red
+//                    Color.blue
+//                }
+//
+//                Text("Your content")
+//                    .foregroundStyle(.secondary)
+//                    .padding(50)
+//                    .background(.ultraThinMaterial)
+//            }
+//        }
+//        .background(.red)
+//        .padding()
+//        .ignoresSafeArea()
     }
 }
 
